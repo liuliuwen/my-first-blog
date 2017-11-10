d0c11f009799ddec14b0af007631fd5df1e21474
liuliuwen <cl.liuliuwen@gmail.com>
2017-11-11 00:00:01 +0800

    Changed the HTML for the site.

--- mysite/settings.py
+++ mysite/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'n!m*9)1o!m*6o8^$71tgxs7503eq45nd+b6z@-w#a(_wzi^j1%'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['liuliuwen.pythonanywhere.com']
+ALLOWED_HOSTS = ['liuliuwen.pythonanywhere.com', '127.0.0.1']
 
 
 # Application definition
